531affc8875db95dbda010c1286e3a5a2f91825a
Mazy <twt_ios@163.com>
2017-01-05 15:45:45 +0800

    初始化项目

---
+++ XMPopupListView/XMPopupListView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPopupListView
+//
+//  Created by TwtMac on 17/1/5.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPopupListView/XMPopupListView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPopupListView
+//
+//  Created by TwtMac on 17/1/5.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
